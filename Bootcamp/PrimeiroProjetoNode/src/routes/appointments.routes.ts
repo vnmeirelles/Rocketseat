5352fc4316cb8e5ae3d482a00fd2f38929d33ea8
Vinicius Meirelles <vnmeirelles@gmail.com>
2020-12-12 15:33:45 -0300

    Primeiro Projeto com NodeJs/TypeScript.

---
+++ Bootcamp/PrimeiroProjetoNode/src/routes/appointments.routes.ts
@@ -0,0 +1,38 @@
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRepository from '../reposittories/AppointmentsRepository';
+import Appointment from '../models/Appointtment';
+
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parseDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parseDate,
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parseDate,
+  });
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
