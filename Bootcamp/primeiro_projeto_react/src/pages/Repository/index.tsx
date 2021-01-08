3e35555b930ff485530859aca50d1cde1334b4ff
Vinicius Meirelles <vnmeirelles@gmail.com>
2021-01-07 23:21:16 -0300

    Estilizando Detalhes

--- Bootcamp/primeiro_projeto_react/src/pages/Repository/index.tsx
+++ Bootcamp/primeiro_projeto_react/src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronsLeft,FiChevronRight } from 'react-icons/fi';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+import logoImg from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +12,51 @@ interface RepositoryParams {
 const Dashboard: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <div>Repository:{params.repository}</div>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronsLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="https://avatars3.githubusercontent.com/u/40375673?v=4" alt="vnmeirelles" />
+          <div>
+            <strong>rock</strong>
+            <p>descrição</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Star</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues Aberta</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to='asfdasf'>
+          <div>
+            <strong>repository.full_name</strong>
+            <p>repository.description</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Dashboard;
