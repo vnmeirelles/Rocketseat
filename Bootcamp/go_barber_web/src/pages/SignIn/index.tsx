ec36ecdd47f9cc59fa6e2d68e8807bbf8ec1c297
Vinicius Meirelles <vnmeirelles@gmail.com>
2021-11-09 22:38:31 -0300

    Criando hook de toast

--- Bootcamp/go_barber_web/src/pages/SignIn/index.tsx
+++ Bootcamp/go_barber_web/src/pages/SignIn/index.tsx
@@ -4,7 +4,8 @@ import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
-import { useAuth } from '../../hooks/AuthContext';
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
 
 import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
@@ -23,6 +24,7 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
   const { signIn } = useAuth();
+  const { addToast } = useToast();
 
   const handleSubmit = useCallback(async (data: SignInFormData) => {
     try {
@@ -37,7 +39,7 @@ const SignIn: React.FC = () => {
         abortEarly: false,
       });
 
-      signIn({
+      await signIn({
         email: data.email,
         password: data.password,
       });
@@ -48,9 +50,9 @@ const SignIn: React.FC = () => {
         formRef.current?.setErrors(errors);
       }
 
-      // Disparar um Toast
+      addToast();
     }
-  }, [signIn]);
+  }, [signIn, addToast]);
 
   return (
     <Container>
