b45324a263acb4384ec6c66e75a1518a8d192c17
Vinicius Meirelles <vnmeirelles@gmail.com>
2022-01-10 22:55:17 -0300

    Animando Toasts

--- Bootcamp/go_barber_web/src/App.tsx
+++ Bootcamp/go_barber_web/src/App.tsx
@@ -4,7 +4,6 @@ import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
-import ToastContainer from './components/ToastContainer';
 import AppProvider from './hooks';
 
 const App: React.FC = () => (
