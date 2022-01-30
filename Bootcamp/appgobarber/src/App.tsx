faa866a680a42cc97c75e936b6ff5ec8971b4303
Vinicius Meirelles <vnmeirelles@gmail.com>
2022-01-30 16:10:50 -0300

    Ajuste de Warning

--- Bootcamp/appgobarber/src/App.tsx
+++ Bootcamp/appgobarber/src/App.tsx
@@ -1,10 +1,16 @@
-// import 'react-native-gesture-handler';
-
+import 'react-native-gesture-handler';
 import React from 'react';
 import {StatusBar, View} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Routes  from './routes';
+
+// Verificar um caminho melhor para incluir essas instrução
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs([
+  "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!",
+]);
+// Verificar um caminho melhor para incluir essas instrução
 
 const App:   React.FC = () => (
   <NavigationContainer>
