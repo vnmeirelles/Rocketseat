832675fc6b7a7a369dc546b9902d770d848d6628
Vinicius Meirelles <vnmeirelles@gmail.com>
2022-03-29 22:54:06 -0300

    Separando em módulos, Camada de Infra, Configurando Imports

--- Bootcamp/GoBarber_BackEnd/src/modules/appointments/infra/typeorm/entities/Appointment.ts
+++ Bootcamp/GoBarber_BackEnd/src/modules/appointments/infra/typeorm/entities/Appointment.ts
@@ -6,7 +6,7 @@ import {Entity,
         ManyToOne,
         JoinColumn} from 'typeorm';
 
-import User from './User';
+import User from '@modules/users/infra/typeorm/entities/User';
 @Entity('appointments')
 class Appointment {
   @PrimaryGeneratedColumn('uuid')
